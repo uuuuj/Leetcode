d0f14d78741044eb03263ed0a696ad8a9f931386
uuuuj <yjth4436@gmail.com>
2023-10-29 15:06:24 +0900

    34번 bisect 사용 안한 정답 코드!이전 커밋은 예외처리 안되어 있음. 지금은 예외처리 완

--- 34.py
+++ 34.py
@@ -3,19 +3,23 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         left = 0
         right = len(nums)-1
+        if nums == []:
+            return [-1, -1]
         while True:
             mid = (left + right) // 2
             if nums[mid] == target:
                 break
             if nums[mid] < target:
                 left = mid + 1
+            if left > right:
+                return [-1, -1]
             elif nums[mid] > target:
                 right = mid - 1
         while nums[left] != target:
             left += 1
         while nums[right] != target:
             right -= 1
-        return right - left + 1
+        return [left, right]
 sol = Solution()
 nums = [5, 7, 7, 8, 8, 10]
 target = 8
